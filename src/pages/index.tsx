75b68fbcf921cfacde75d4b83b81378517535688
Felipe Marinho <felipmarinho@gmail.com>
2021-08-03 09:00:06 -0300

    change SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import React from 'react';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
-
 import styles from './home.module.scss';
+
 
 interface HomeProps {
   product: {
@@ -38,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JKJ80FzrUvWUivTzDYZwG0G', {
     expand: ['product'],
   });
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   };
 };
